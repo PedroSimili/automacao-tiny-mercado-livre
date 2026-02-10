9fe45e3ba2f3a7d0ba9abf1a41a83e35b532ea40
Pedro Simili Alcantara <pedrosimili10@gmail.com>
2026-02-10 16:37:10 -0300

    feat: deixando o cogido mais limpo e estruturado com def e class

--- automation/playwright_bot.py
+++ automation/playwright_bot.py
@@ -1,4 +1,4 @@
-'''
+﻿'''
 Automação de Gerenciamento de Anúncios no Tiny ERP.
 
 Este script utiliza Playwright para automatizar o login no sistema Tiny, 
@@ -17,102 +17,116 @@ import time
 
 load_dotenv()
 
+class TinyAutomation:
+    def __init__(self, pagina):
+        self.pagina = pagina
+        self.url_site = os.getenv('url_site')
+        self.login_usuario = os.getenv('login')
+        self.senha_usuario = os.getenv('senha')
 
-def tempo_de_espera():
-    time.sleep(3)
-    
+    def tempo_de_espera(self, segundos=3):
+        time.sleep(segundos)
+        
 
-def opcao_do_usuario():
-     print('\n-----MENU DE ESCOLHA-----')
-     print('1. Mercado Livre 1')
-     print('2. Mercado Livre 2')
-     print('3. Mercado Livre 3')
-     escolha = input('Digite o número de de qual ML você quer ir')
-     return escolha
-escolha = opcao_do_usuario()
+    def opcao_do_usuario(self):
+        print('\n-----MENU DE ESCOLHA-----')
+        print('1. Mercado Livre 1')
+        print('2. Mercado Livre 2')
+        print('3. Mercado Livre 3')
+        escolha = input('Digite o número de de qual ML você quer ir')
+        return escolha
+    #escolha = opcao_do_usuario()
 
-def Sua_MLBs():
-    print('\n-----DIGITE SUA(s) MLBs-----')
-    MLBs = input('Digite suas MLB(s)')
-    return MLBs
-MLBs = Sua_MLBs()
+    def login(self):
+        self.pagina.goto(self.url_site)
+        
+        self.pagina.get_by_role("textbox", name="usuário").fill(self.login_usuario)
+        self.tempo_de_espera()
+        self.pagina.get_by_role("textbox", name="senha").fill(self.senha_usuario)
+        self.tempo_de_espera()
+        self.pagina.get_by_role("button", name="Entrar").click()
+        self.tempo_de_espera()
+        self.pagina.get_by_role("button", name="login").click()
+        self.pagina.locator(".btn-sidebar-menu").click()
+        self.pagina.get_by_role("link", name="Cadastros").click()
+        self.pagina.get_by_role("link", name="Anúncios").click()
+        time.sleep(6)
 
-def _logout():
-    pagina.get_by_role("link", name="Menu Usuário").click()
-    tempo_de_espera()
-    pagina.get_by_role("link", name="Sair").click()
-    tempo_de_espera()
+    def ir_ao_ml(self, escolha):
+        try:
+            print(f'Voce escolheu a opcao {escolha}, vamos abrir o mercado livre {escolha}')
+            
+            if escolha == '1':
+                self.pagina.get_by_role("link", name="gerenciar").first.click()
+            elif escolha == '2':
+                self.pagina.get_by_role("link", name="gerenciar").nth(1).click()
+            elif escolha == '3':
+                self.pagina.get_by_role("link", name="gerenciar").nth(2).click()
+            print('deu certo')
+        except Exception as e:
+            print(f'ERRO: {e}')
+            
+    def importar_para_ml(self, MLBs):
+        self.pagina.get_by_role("button", name="Mais ações").click()   
+        self.pagina.get_by_role("link", name="Importar anúncios").click()
+        self.pagina.get_by_text("um anúncio específico").click()
+        self.pagina.locator("input[name=\"identificadorAnuncio\"]").click()
+        self.pagina.locator("input[name=\"identificadorAnuncio\"]").fill(MLBs)
+        self.pagina.get_by_role("button", name="Prosseguir").click()
+        self.tempo_de_espera()
+        self.pagina.get_by_role("button", name="Fechar").click()
+        
+    def relacionar_ml(self):
+        self.pagina.get_by_role("link", name="filtros").click()
+        self.tempo_de_espera()
+        self.pagina.locator("#filtroRelacionados").select_option("N")
+        self.pagina.get_by_role("button", name="Aplicar").click()
+        self.tempo_de_espera()
+        self.pagina.get_by_role("columnheader").first.click()
+        self.tempo_de_espera()
+        self.pagina.get_by_role("button", name="Mais ações").nth(1).click()
+        self.pagina.get_by_role("link", name="Relacionar anúncios").click()
+        self.pagina.get_by_role("button", name="Relacionar").click()
+        self.tempo_de_espera()
+        #self.pagina.get_by_role("button", name="Fechar", exact=True).click()
+        self.pagina.get_by_role("button", name="fechar ").click()
+        
+    def Sua_MLBs(self):
+        print('\n-----DIGITE SUA(s) MLBs-----')
+        MLBs = input('Digite suas MLB(s)')
+        return MLBs
+    #MLBs = Sua_MLBs()
+
+    def logout(self):
+        self.pagina.get_by_role("link", name="Menu Usuário").click()
+        self.tempo_de_espera()
+        self.pagina.get_by_role("link", name="Sair").click()
+        self.tempo_de_espera()
         
     
     
-    
-with sync_playwright() as pw: 
-    navegador = pw.chromium.launch(headless=False)
-    contexto = navegador.new_context()
-    
-    pagina = contexto.new_page()
+def main():    
+    with sync_playwright() as pw: 
+        navegador = pw.chromium.launch(headless=False)
+        contexto = navegador.new_context()
 
-    url_site = os.getenv('url_site')
-    login_usuario = os.getenv('login')
-    senha_usuario = os.getenv('senha')
-    
-    pagina.goto(url_site)
-    
-    pagina.get_by_role("textbox", name="usuário").fill(login_usuario)
-    tempo_de_espera()
-    pagina.get_by_role("textbox", name="senha").fill(senha_usuario)
-    tempo_de_espera()
-    pagina.get_by_role("button", name="Entrar").click()
-    tempo_de_espera()
-    pagina.get_by_role("button", name="login").click()
-    pagina.locator(".btn-sidebar-menu").click()
-    pagina.get_by_role("link", name="Cadastros").click()
-    pagina.get_by_role("link", name="Anúncios").click()
-    time.sleep(6)
-    try:
-        
-        print(f'Voce escolheu a opcao {escolha}, vamos abrir o mercado livre {escolha}')
-        
-        if escolha == '1':
-            pagina.get_by_role("link", name="gerenciar").first.click()
-        elif escolha == '2':
-            pagina.get_by_role("link", name="gerenciar").nth(1).click()
-        elif escolha == '3':
-            pagina.get_by_role("link", name="gerenciar").nth(2).click()
-        print('deu certo')
-    except Exception as e:
-        print(f'ERRO: {e}')
-        
-    pagina.get_by_role("button", name="Mais ações ").click()   
-    pagina.get_by_role("link", name=" Importar anúncios").click()
-    pagina.get_by_text("um anúncio específico").click()
-    pagina.locator("input[name=\"identificadorAnuncio\"]").click()
-    pagina.locator("input[name=\"identificadorAnuncio\"]").fill(MLBs)
-    pagina.get_by_role("button", name="Prosseguir").click()
-    tempo_de_espera()
-    pagina.get_by_role("button", name="Fechar").click()
-    
-    
-    pagina.get_by_role("link", name=" filtros").click()
-    tempo_de_espera()
-    pagina.locator("#filtroRelacionados").select_option("N")
-    pagina.get_by_role("button", name="Aplicar").click()
-    tempo_de_espera()
-    pagina.get_by_role("columnheader").first.click()
-    tempo_de_espera()
-    pagina.get_by_role("button", name="Mais ações ").nth(1).click()
-    pagina.get_by_role("link", name=" Relacionar anúncios").click()
-    pagina.get_by_role("button", name="Relacionar").click()
-    tempo_de_espera()
-    pagina.get_by_role("button", name="Fechar", exact=True).click()
-    pagina.get_by_role("button", name="fechar ").click()
-    tempo_de_espera()
-    
-    _logout()
+        pagina = contexto.new_page()
+        automacao = TinyAutomation(pagina)
 
-        
+        automacao.login()
 
-
-    tempo_de_espera()
-    navegador.close()
+        escolha = automacao.opcao_do_usuario()
+         
+        automacao.ir_ao_ml(escolha)
     
+        MLBs = automacao.Sua_MLBs()
+        
+        automacao.importar_para_ml(MLBs)
+    
+        automacao.relacionar_ml()
+    
+        automacao.logout()
+
+        navegador.close()
+if __name__ == '__main__':
+    main()
--- automation/readme.md
+++ automation/readme.md
@@ -1,3 +1,7 @@
 # Sou estudante de Cybersecurity e estou explorando o universo do desenvolvimento. Meu primeiro projeto é um bot de automação focado em otimizar processos manuais em instituições. Pretendo evoluir este código continuamente à medida que aprofundo meus estudos, tornando-o cada vez mais robusto e profissional.
 #
 # Neste projeto irei utilizar o Playwright um bot de automação bem consolidade que bate de igual para igual com o Selenium. E para conseguir me comunicar com o Playwright irei utilizar a linguagem Python, que tenho mais afinidade e que considero uma das mais utilizadas no mundo da CyberSecurity.
+#
+# I am a Cybersecurity student exploring the world of software development. My first project is an automation bot designed to optimize manual processes within institutions. I plan to continuously evolve this code as I deepen my studies, making it increasingly robust and professional.
+#
+# For this project, I will be using Playwright, a well-established automation framework that stands toe-to-toe with Selenium. To interface with Playwright, I’ll be using Python—a language I’m highly comfortable with and one of the most widely used in the Cybersecurity field.
