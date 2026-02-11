8f14b46f9c1b11af55dde61874cc6370022d14fb
Pedro Simili Alcantara <pedrosimili10@gmail.com>
2026-02-11 09:34:20 -0300

    feat: implementa biblioteca logging e blocos try-except para melhor rastreabilidade

--- automation/playwright_bot.py
+++ automation/playwright_bot.py
@@ -12,8 +12,17 @@ import os
 from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
 import time
+import logging
 
-
+logging.basicConfig(
+    level= logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler('automacai.log', encoding='utf-8'),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -29,31 +38,46 @@ class TinyAutomation:
         
 
     def opcao_do_usuario(self):
-        print('\n-----MENU DE ESCOLHA-----')
-        print('1. Mercado Livre 1')
-        print('2. Mercado Livre 2')
-        print('3. Mercado Livre 3')
-        escolha = input('Digite o número de de qual ML você quer ir')
-        return escolha
+        try:
+            logger.info('Inciando programa')
+            print('\n-----MENU DE ESCOLHA-----')
+            print('1. Mercado Livre 1')
+            print('2. Mercado Livre 2')
+            print('3. Mercado Livre 3')
+            escolha = input('Digite o número de de qual ML você quer ir')
+            return escolha
+
+            logger.info('Opção armazenada com sucesso')
+        except Exception:
+            logger.exception('Erro ao armanezar a opção')
+            raise
     #escolha = opcao_do_usuario()
 
     def login(self):
-        self.pagina.goto(self.url_site)
-        
-        self.pagina.get_by_role("textbox", name="usuário").fill(self.login_usuario)
-        self.tempo_de_espera()
-        self.pagina.get_by_role("textbox", name="senha").fill(self.senha_usuario)
-        self.tempo_de_espera()
-        self.pagina.get_by_role("button", name="Entrar").click()
-        self.tempo_de_espera()
-        self.pagina.get_by_role("button", name="login").click()
-        self.pagina.locator(".btn-sidebar-menu").click()
-        self.pagina.get_by_role("link", name="Cadastros").click()
-        self.pagina.get_by_role("link", name="Anúncios").click()
-        time.sleep(6)
+        try:
+            logger.info('Iniciando Login')
+            
+            self.pagina.goto(self.url_site)
+            
+            self.pagina.get_by_role("textbox", name="usuário").fill(self.login_usuario)
+            self.tempo_de_espera()
+            self.pagina.get_by_role("textbox", name="senha").fill(self.senha_usuario)
+            self.tempo_de_espera()
+            self.pagina.get_by_role("button", name="Entrar").click()
+            self.tempo_de_espera()
+            self.pagina.get_by_role("button", name="login").click()
+            self.pagina.locator(".btn-sidebar-menu").click()
+            self.pagina.get_by_role("link", name="Cadastros").click()
+            self.pagina.get_by_role("link", name="Anúncios").click()
+            time.sleep(6)
+            logger.info('Login feito com sucesso!!!')
+        except Exception:
+            logger.exception('Erro no Login')
+            raise
 
     def ir_ao_ml(self, escolha):
         try:
+            logger.info(f'Indo para o ML{escolha}')
             print(f'Voce escolheu a opcao {escolha}, vamos abrir o mercado livre {escolha}')
             
             if escolha == '1':
@@ -62,71 +86,99 @@ class TinyAutomation:
                 self.pagina.get_by_role("link", name="gerenciar").nth(1).click()
             elif escolha == '3':
                 self.pagina.get_by_role("link", name="gerenciar").nth(2).click()
-            print('deu certo')
-        except Exception as e:
-            print(f'ERRO: {e}')
+            logger.info(f'opção ML{escolha} feita com sucesso')
+        except Exception:
+            logger.exception('Erro ao tentar ir ao ML')
+            raise
             
     def importar_para_ml(self, MLBs):
-        self.pagina.get_by_role("button", name="Mais ações").click()   
-        self.pagina.get_by_role("link", name="Importar anúncios").click()
-        self.pagina.get_by_text("um anúncio específico").click()
-        self.pagina.locator("input[name=\"identificadorAnuncio\"]").click()
-        self.pagina.locator("input[name=\"identificadorAnuncio\"]").fill(MLBs)
-        self.pagina.get_by_role("button", name="Prosseguir").click()
-        self.tempo_de_espera()
-        self.pagina.get_by_role("button", name="Fechar").click()
+        try:
+            logger.info('Inciando processo de importação...')
+            self.pagina.get_by_role("button", name="Mais ações").click()   
+            self.pagina.get_by_role("link", name="Importar anúncios").click()
+            self.pagina.get_by_text("um anúncio específico").click()
+            self.pagina.locator("input[name=\"identificadorAnuncio\"]").click()
+            self.pagina.locator("input[name=\"identificadorAnuncio\"]").fill(MLBs)
+            self.pagina.get_by_role("button", name="Prosseguir").click()
+            self.tempo_de_espera()
+            self.pagina.get_by_role("button", name="Fechar").click()
+            logger.info('Importação feita com sucesso')
+        except Exception:
+            logger.exception('Erro ao realizar a importação')
+            raise
         
-    def relacionar_ml(self):
-        self.pagina.get_by_role("link", name="filtros").click()
-        self.tempo_de_espera()
-        self.pagina.locator("#filtroRelacionados").select_option("N")
-        self.pagina.get_by_role("button", name="Aplicar").click()
-        self.tempo_de_espera()
-        self.pagina.get_by_role("columnheader").first.click()
-        self.tempo_de_espera()
-        self.pagina.get_by_role("button", name="Mais ações").nth(1).click()
-        self.pagina.get_by_role("link", name="Relacionar anúncios").click()
-        self.pagina.get_by_role("button", name="Relacionar").click()
-        self.tempo_de_espera()
-        #self.pagina.get_by_role("button", name="Fechar", exact=True).click()
-        self.pagina.get_by_role("button", name="fechar ").click()
+    def relacionar_ml(self, escolha):
+        try:
+            logger.info(f'Iniciando processo de relacionar ao ml{escolha}')
+            self.pagina.get_by_role("link", name="filtros").click()
+            self.tempo_de_espera()
+            self.pagina.locator("#filtroRelacionados").select_option("N")
+            self.pagina.get_by_role("button", name="Aplicar").click()
+            self.tempo_de_espera()
+            self.pagina.get_by_role("columnheader").first.click()
+            self.tempo_de_espera()
+            self.pagina.get_by_role("button", name="Mais ações").nth(1).click()
+            self.pagina.get_by_role("link", name="Relacionar anúncios").click()
+            self.pagina.get_by_role("button", name="Relacionar").click()
+            self.tempo_de_espera()
+            #self.pagina.get_by_role("button", name="Fechar", exact=True).click()
+            self.pagina.get_by_role("button", name="fechar ").click()
+            logger.info(f'Processo de relacionar feito com sucesso ao ml{escolha}')
+        except Exception:
+            logger.exception('Erro no processo de relacionar')
+            raise
         
     def Sua_MLBs(self):
-        print('\n-----DIGITE SUA(s) MLBs-----')
-        MLBs = input('Digite suas MLB(s)')
-        return MLBs
+        try:
+            logger.info('Iniciando processo de coleta das MLB(s)')
+            print('\n-----DIGITE SUA(s) MLBs-----')
+            MLBs = input('Digite suas MLB(s)')
+            return MLBs
+            logger.info('MLB(s) coletadas com sucesso')
+        except Exception:
+            logger.exception('Erro ao coletar as MLB(s)')
+            raise
     #MLBs = Sua_MLBs()
 
     def logout(self):
-        self.pagina.get_by_role("link", name="Menu Usuário").click()
-        self.tempo_de_espera()
-        self.pagina.get_by_role("link", name="Sair").click()
-        self.tempo_de_espera()
+        try:
+            logger.info('Iniciando processo de logout')
+            self.pagina.get_by_role("link", name="Menu Usuário").click()
+            self.tempo_de_espera()
+            self.pagina.get_by_role("link", name="Sair").click()
+            self.tempo_de_espera()
+            logger.info('Logout realizado com sucesso')
+        except Exception:
+            logger.exception('Erro ao tentar fazer o logout')
+            raise
+            
+def main(): 
+    try:
+        logger.info('Iniciando automação')  
+        with sync_playwright() as pw: 
+            navegador = pw.chromium.launch(headless=False)
+            contexto = navegador.new_context()
+
+            pagina = contexto.new_page()
+            automacao = TinyAutomation(pagina)
+
+            automacao.login()
+
+            escolha = automacao.opcao_do_usuario()
+            
+            automacao.ir_ao_ml(escolha)
         
-    
-    
-def main():    
-    with sync_playwright() as pw: 
-        navegador = pw.chromium.launch(headless=False)
-        contexto = navegador.new_context()
-
-        pagina = contexto.new_page()
-        automacao = TinyAutomation(pagina)
-
-        automacao.login()
-
-        escolha = automacao.opcao_do_usuario()
-         
-        automacao.ir_ao_ml(escolha)
-    
-        MLBs = automacao.Sua_MLBs()
+            MLBs = automacao.Sua_MLBs()
+            
+            automacao.importar_para_ml(MLBs)
         
-        automacao.importar_para_ml(MLBs)
-    
-        automacao.relacionar_ml()
-    
-        automacao.logout()
+            automacao.relacionar_ml(escolha)
+        
+            automacao.logout()
 
-        navegador.close()
+            navegador.close()
+            logger.info('Automação finalizada com sucesso')
+    except Exception:
+        logger.exception('Erro em na automação')
 if __name__ == '__main__':
     main()
